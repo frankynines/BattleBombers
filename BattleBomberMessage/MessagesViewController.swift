6759092c507b710e70ace04b08f39f0fa459c3c3
Franky Aguilar <knarfff@Frankys-MacBook-Pro.local>
2019-02-17 08:28:47 -0700

    Message Session

--- BattleBomberMessage/MessagesViewController.swift
+++ BattleBomberMessage/MessagesViewController.swift
@@ -12,8 +12,7 @@ import Celer
 import QRCode
 import SpriteKit
 
-class MessagesViewController: MSMessagesAppViewController, GameSceneProtocol {
-   
+class MessagesViewController: MSMessagesAppViewController, GameSceneProtocol ,CelerGroupClientCallback {
    
     @IBOutlet weak var ibo_qrCode:UIImageView?
     @IBOutlet weak var ibo_publicKey:UILabel?
@@ -21,71 +20,104 @@ class MessagesViewController: MSMessagesAppViewController, GameSceneProtocol {
     var ibo_explosion:UIImageView?
     var gameScene:GameScene?
     var currentPlayer = "1"
-    
+    private var stake: String = "0"
+    private let keystorePass = " "
+
     @IBOutlet var ibo_spriteView:SKView?
     var session:MSSession?
     
+    @IBOutlet var ibo_networkStatus:UILabel?
+    @IBOutlet var ibo_gameID:UILabel?
+
+    var gameSessionID:String?
+    var gameJoin: Bool = false
+    var playerAddress:String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.iba_createWallet()
+        self.setupWallet()
+
         if let scene = GameScene(fileNamed: "GameScene") {
-            // Set the scale mode to scale to fit the window
             self.gameScene = scene
             self.gameScene?.protocolDelegate = self
             self.ibo_spriteView?.presentScene(scene)
         }
+        
+        print("View Did Load")
+        //self.iba_initializeCeler()
+        
     }
     
-    func didChangeState() {
-        print("Changed")
+    override func willBecomeActive(with conversation: MSConversation) {
+
+        if let messageURL = conversation.selectedMessage?.url {
+            self.session = conversation.selectedMessage?.session
+            print(messageURL)
+            self.gameJoin = true
+            self.playerAddress = self.getQueryStringParameter(url: messageURL.absoluteString, param: "publicAddress")
+            self.gameScene?.loadGameState(loadState: messageURL.absoluteString)
+           // self.iba_initializeCeler()
+        } else {
+            //
+        }
+    }
+    func getQueryStringParameter(url: String, param: String) -> String? {
+        guard let url = URLComponents(string: url) else { return nil }
+        return url.queryItems?.first(where: { $0.name == param })?.value
     }
     
-    @IBAction func iba_createWallet() {
+//    @IBAction func iba_checkCelerBalance() {
+//        let networkBalance = CelerClientAPIHelper.shared.checkBalance()
+//    }
+    
+    func setupWallet() {
         do {
             if EtherWallet.account.hasAccount == false {
-                try EtherWallet.account.generateAccount(password: "")
+                try EtherWallet.account.generateAccount(password: keystorePass)
             }
             
             let qrCode = QRCode(EtherWallet.account.address!)
             self.ibo_publicKey?.text = EtherWallet.account.address!
             ibo_qrCode?.image = qrCode!.image
-            
+            self.updateBalance()
         } catch {
             print(error)
         }
     }
     
-    @IBAction func iba_plantBomb(sender:UIButton){
-        sender.setImage(UIImage(named: "plot_plant.png"), for: .normal)
-    
+    func updateBalance() {
+        let balance = try? EtherWallet.balance.etherBalanceSync()
+        self.ibo_networkStatus?.text = balance
     }
-    
+
     func prepareUrl(state: [String:Any]) -> URL {
         var urlComponents = URLComponents()
         
         urlComponents.queryItems = []
+        let item = URLQueryItem(
+            name: "publicAddress",
+            value:EtherWallet.account.address!)
+        urlComponents.queryItems?.append(item)
         
         for item in state {
 
             let item = URLQueryItem(name: item.key, value:(item.value as! String))
             urlComponents.queryItems?.append(item)
         }
-//        print("URL: ", urlComponents.url!)
         return urlComponents.url!
     }
     
-    @IBAction func iba_submitMove() {
+    func iba_submitMove() {
+        if !(self.session != nil) {
+            self.session = MSSession()
+        }
         let savedStateArray = self.gameScene?.saveGameStates()
-
-        
         let gameURL = prepareUrl(state:savedStateArray!)
-    
         let layout = MSMessageTemplateLayout()
         layout.image = UIImage(named: "bomb.png")
         layout.caption = "Let's play battle bombers"
         layout.subcaption = "Can you find the Bufficorn?"
-        let message = MSMessage()
+        let message = MSMessage(session: session!)
         message.url = gameURL
         message.layout = layout
 
@@ -94,19 +126,7 @@ class MessagesViewController: MSMessagesAppViewController, GameSceneProtocol {
                 print(error)
             }
         }
-
         self.dismiss()
-    }
-    
-    func getScreenshot(scene: SKScene) -> UIImage {
-        let snapshotView = self.ibo_spriteView!.snapshotView(afterScreenUpdates: true)
-        let bounds = self.ibo_spriteView?.bounds
-        
-        UIGraphicsBeginImageContextWithOptions(bounds!.size, false, 0)
-        snapshotView?.drawHierarchy(in: bounds!, afterScreenUpdates: true)
-        let screenshotImage : UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        return screenshotImage;
     }
     
     func did_FinishSettingUpGame() {
@@ -116,14 +136,15 @@ class MessagesViewController: MSMessagesAppViewController, GameSceneProtocol {
     
     func did_loseGame() {
         print("MOVE FINISH")
-        let alert = UIAlertController(title: "You Lost", message: nil, preferredStyle: .alert)
+        let alert = UIAlertController(title: "You Lost - 0.1 ETH", message: nil, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { (action) in
-            
+            EtherWallet.transaction.sendEther(to: self.playerAddress!, amount: "0.1", password: " ", completion: { (stunt) in
+                //
+            })
         }))
         self.present(alert, animated: true) {
             self.sendMessageWithSummary(string: "I Lost, Heres your Eth!")
             print("I lost")
-
         }
     }
     
@@ -141,72 +162,106 @@ class MessagesViewController: MSMessagesAppViewController, GameSceneProtocol {
     func sendMessageWithSummary(string:String) {
         let layout = MSMessageTemplateLayout()
         
-        let message = MSMessage()
+        let message = MSMessage(session: session!)
         message.layout = layout
         message.summaryText = string
         print("sumtext", message);
         
-        self.activeConversation!.insert(message) { error in
-            if let error = error {
-                print(error)
+self.presentationStyle = 
+//        self.dismiss()
+    }
+
+    //CELER GOODIES
+    private var groupClient: CelerGroupClient? = nil
+
+    //01
+    func iba_initializeCeler() {
+        let keystore = KeystoreHelper.shared.returnKeystore()
+        
+        CelerClientAPIHelper.shared.initCelerClient(
+            keyStoreString: keystore,
+            password: keystorePass
+        ) { (done) in
+            if done {
+                self.joinCelerNetwork()
+            } else {
+                self.ibo_networkStatus?.text = "FAILED"
             }
         }
+    }
+    //02
+    func joinCelerNetwork() {
+            CelerClientAPIHelper.shared.joinCeler(
+                clientSideDepositAmount: "0.1",
+                serverSideDepositAmount: "0.1") { (done) in
+                    if done {
+                        self.ibo_networkStatus?.text = "Status: Connected"
+                        self.createCelerGame()
+                    } else {
+                        self.ibo_networkStatus?.text = "Status: ERROR"
+                    }
+            }
+    }
+    //03
+    func createCelerGame() {
         
-        self.dismiss()
+        self.initializeGameClient { (done) in
+            //
+            groupClient?.createGameFrom(userAddress: EtherWallet.account.address!, withStake: "0.1", forNumberOfPlayers: 2, errorHandler: { (error) in
+                print(error)
+            })
+        }
+        
+    }
+    //04
+    func  initializeGameClient(completion: (_ results: Bool) ->()) {
+        let keystore = KeystoreHelper.shared.returnKeystore()
+        
+        groupClient = CelerGroupClient(
+            serverAdress: "group-hack-ropsten.celer.app:10001",
+            keystoreJSON: keystore,
+            password:keystorePass) { error in
+                
+                DispatchQueue.main.async {
+                    print(error.localizedDescription)
+                }
+        }
+        groupClient?.delegate = self
     }
     
-    // MARK: - Conversation Handling
     
-    override func willBecomeActive(with conversation: MSConversation) {
+    func joinGame() {
         
-        if let messageURL = conversation.selectedMessage?.url {
-            print(messageURL)
-            self.gameScene?.loadGameState(loadState: messageURL.absoluteString)
+//        groupClient?.joinGame(
+//            userAddress: EtherWallet.account.address!,
+//            withGameCode: Int(self.joinGameSessionID!)!,
+//            withStake: "0.1") { error in
+//        }
+    }
+    
+    func onSuccess(_ response: CelerGroupResponse) {
+        
+        if (response.getUsers().components(separatedBy: ",").count == 2) {
+            DispatchQueue.main.async {
+                print(response)
+            }
         } else {
-            
-            //NEW GAME
+            DispatchQueue.main.async {
+                self.stake = response.getStake()
+                self.gameSessionID = String(response.getGameCode())
+                self.ibo_gameID?.text = String(response.getGameCode())
+                if (self.gameJoin) {
+                    //self.ibo_gameID?.text = self.joinGameSessionID
+                    //self.joinGame()
+                }
+            }
         }
     }
     
-    
-    
-    override func didResignActive(with conversation: MSConversation) {
-        // Called when the extension is about to move from the active to inactive state.
-        // This will happen when the user dissmises the extension, changes to a different
-        // conversation or quits Messages.
-        
-        // Use this method to release shared resources, save user data, invalidate timers,
-        // and store enough state information to restore your extension to its current state
-        // in case it is terminated later.
-    }
-   
-    override func didReceive(_ message: MSMessage, conversation: MSConversation) {
-        // Called when a message arrives that was generated by another instance of this
-        // extension on a remote device.
-        
-        // Use this method to trigger UI updates in response to the message.
-    }
-    
-    override func didStartSending(_ message: MSMessage, conversation: MSConversation) {
-        // Called when the user taps the send button.
-    }
-    
-    override func didCancelSending(_ message: MSMessage, conversation: MSConversation) {
-        // Called when the user deletes the message without sending it.
-    
-        // Use this to clean up state related to the deleted message.
-    }
-    
-    override func willTransition(to presentationStyle: MSMessagesAppPresentationStyle) {
-        // Called before the extension transitions to a new presentation style.
-    
-        // Use this method to prepare for the change in presentation style.
-    }
-    
-    override func didTransition(to presentationStyle: MSMessagesAppPresentationStyle) {
-        // Called after the extension transitions to a new presentation style.
-    
-        // Use this method to finalize any behaviors associated with the change in presentation style.
+    func onFailure(_ error: Error, _ description: String) {
+        DispatchQueue.main.async {
+            print("ERROR", error.localizedDescription)
+        }
     }
 
 }
